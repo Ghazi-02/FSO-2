3fc91f2012bc8840d6098d4a92dfa7989d7cd094
Areeb <auhmirza02@gmail.com>
2022-10-26 17:05:09 +0100

    2.5: separate module

--- courseinfo2/src/App.js
+++ courseinfo2/src/App.js
@@ -1,48 +1,7 @@
 
+import Course from "./components/course"
 
 
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Part = ({ part }) => {
- 
-  return (
-    <div>
-      {part.name} {part.exercises}
-    </div>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(item => (<Part part={item} key={item.id} />))}
-    </div>)
-}
-
-const Course = (props) => {
-
-
-  return (
-    <div>
-
-      <Header course={props.course.name} />
-      <Content parts={props.course.parts}/>
-      <Total total ={props.course.parts} />
-
-    </div>
-  )
-}
-const Total= ({total}) => {
- 
-
-  return(
-    <div>
-      It has {total.reduce(function (acc, curr) {
-    return acc + curr.exercises
-  }, 0)} exercises
-    </div>
-  )
-}
 
 
 
